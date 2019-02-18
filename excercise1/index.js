f76331f9cf2a1c954283432776ff9d4b03969c6d
ChristophP <deedop@hotmail.de>
2019-02-18 08:06:43 +0100

    install deps and add excercise 1

---
+++ excercise1/index.js
@@ -0,0 +1,13 @@
+// using functions as arguments
+const R = require('ramda')
+
+const identity = a => a;
+
+const array = map(identity, ['this', 'should', 'be', 'uppercase'])
+
+map
+
+
+module.exports = {
+  array,
+};
