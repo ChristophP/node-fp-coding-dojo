cb05459c16e8d579d6f37ec1e552d97f59b9e3b0
Dev24 <MrDev24@gmail.com>
2021-06-28 17:13:56 +1000

    Fixing unit tests for exercise 8

--- excercise8/index.test.js
+++ excercise8/index.test.js
@@ -16,25 +16,25 @@ describe("task", () => {
 
 describe("failedTask", () => {
   test("rejects", () => {
-    return expect(failedTask.run()).rejects;
+    return expect(failedTask.run()).rejects.toEqual('OMG');
   });
 });
 
 describe("mappedTask", () => {
   test("resolves with 8", () => {
-    return expect(task.run()).resolves.toBe(8);
+    return expect(mappedTask.run()).resolves.toBe(8);
   });
 });
 
 describe("getRandom", () => {
   test("generated a random", () => {
-    return expect(task.run()).resolves;
+    return expect(getRandom.run()).resolves.toEqual(expect.any(Number));
   });
 });
 
 describe("fetchPubliIp", () => {
   test("fetches the public ip", () => {
-    return expect(task.run()).resolves.toMatch(/\d{3}\.\d{3}\.\d{3}\.\d/);
+    return expect(fetchPubliIp.run()).resolves.toMatch(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
   });
 });
 
